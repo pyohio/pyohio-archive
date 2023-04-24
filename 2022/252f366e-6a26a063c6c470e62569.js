581c7a4b7043fdb1f58e04a04524894d5cc7e1cb
Dave Forgac <tylerdave@tylerdave.com>
2023-04-24 16:05:56 -0600

    add 2022 archive

---
+++ 2022/252f366e-6a26a063c6c470e62569.js
@@ -0,0 +1,2 @@
+(self.webpackChunkpyohio_static_website=self.webpackChunkpyohio_static_website||[]).push([[228],{2775:function(t,a,i){"use strict";i.d(a,{Faw:function(){return e},Yc6:function(){return r}});var n=i(1046);function e(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"}}]})(t)}function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-6a26a063c6c470e62569.js.map
